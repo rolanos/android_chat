f3e3d4e08df4a7eedda089510be8499fddc998f8
rolanos <ivankson@gmail.com>
2023-07-15 17:36:53 +0300

    update README.md

--- app/src/main/java/API/ApiService.java
+++ app/src/main/java/API/ApiService.java
@@ -1,4 +1,31 @@
 package API;
 
-public interface ApiService {
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ApiService {
+    private final OkHttpClient client;
+
+    public ApiService() {
+        client = new OkHttpClient.Builder().build();
+    }
+
+    public Single<String> makeRequest(String url) {
+        return Single.fromCallable(() -> {
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    if (!response.isSuccessful()) {
+                        throw new RuntimeException("Failed to make request: " + response);
+                    }
+                    return response.body().string();
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
 }
